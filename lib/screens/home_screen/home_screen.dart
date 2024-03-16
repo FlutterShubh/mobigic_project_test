426515628d61530e892cd1c126b0d16a4e824ca4
flutterShubh <shubhamkumarpandey.edugaon@gmail.com>
2024-03-16 11:19:37 +0530

    Update home_screen.dart
    project completed

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -6,10 +6,9 @@ import 'package:mobigic_project/controllers/home_screen_controller/home_screen_c
 import 'package:mobigic_project/utils/utils.dart';
 
 class HomeScreen extends StatefulWidget {
-
-
-  const HomeScreen(
-      {super.key,});
+  const HomeScreen({
+    super.key,
+  });
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -24,8 +23,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -36,81 +33,100 @@ class _HomeScreenState extends State<HomeScreen> {
                 controller: homeScreenController.searchController,
                 leading: Icon(Icons.search),
                 elevation: MaterialStatePropertyAll(0),
-                side: MaterialStatePropertyAll(BorderSide(width: 2,color: Colors.black12)),
+                side: MaterialStatePropertyAll(
+                    BorderSide(width: 2, color: Colors.black12)),
                 hintText: "Search...",
               ),
             ),
             Expanded(
               flex: 1,
-              child:  IconButton(onPressed: () {
-                  homeScreenController.rows.value = 0;
-                  homeScreenController.column.value = 0;
-                  gridBuilderDialog(
+              child: IconButton(
+                  onPressed: () {
+                    homeScreenController.rows.value = 0;
+                    homeScreenController.column.value = 0;
+                    gridBuilderDialog(
                       context: context,
                       columnController: columnController,
                       rowController: rowsController,
                       formKey: formKey,
                       homeScreenController: homeScreenController,
-                    onPressed: () {
-                      if(formKey.currentState!.validate()){
-                        if (int.parse(columnController.text) > 7) {
-                          Get.showSnackbar(GetSnackBar(
-                            message: "Column length should be less than 8",
-                            dismissDirection: DismissDirection.horizontal,
-                          ));
-                        } else {
-                          homeScreenController.rows.value = int.parse(rowsController.text.isEmpty?"0":rowsController.text);
-                          homeScreenController.column.value = int.parse(columnController.text.isEmpty?"0":columnController.text);
-                          homeScreenController.generateCars();
-                          Get.back();
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          if (int.parse(columnController.text) > 7) {
+                            Get.showSnackbar(GetSnackBar(
+                              message: "Column length should be less than 8",
+                              dismissDirection: DismissDirection.horizontal,
+                            ));
+                          } else {
+                            homeScreenController.rows.value = int.parse(
+                                rowsController.text.isEmpty
+                                    ? "0"
+                                    : rowsController.text);
+                            homeScreenController.column.value = int.parse(
+                                columnController.text.isEmpty
+                                    ? "0"
+                                    : columnController.text);
+                            homeScreenController.generateCars();
+                            Navigator.pop(context);
+                          }
                         }
-                      }
-                    },
-                  );
-                }, icon: Icon(Icons.refresh)),
-
+                      },
+                    );
+                  },
+                  icon: Icon(Icons.refresh)),
             ),
           ],
         ),
       ),
-      body: Obx(
-        ()=> Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Expanded(
-            child:homeScreenController.generatedChars.isNotEmpty && (homeScreenController.rows.value > 0)? GridView.builder(
-                gridDelegate:
-                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:  homeScreenController.column.value),
+      body:  Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Obx(
+              () => homeScreenController.generatedChars.isNotEmpty &&
+                (homeScreenController.rows.value > 0)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: homeScreenController.column.value),
                 itemBuilder: (context, index) {
                   // return Text("$rows / $column");
                   return Obx(
-                    ()=> Container(
-                        width: (screenWidth(context) ~/  homeScreenController.column.value).toDouble(),
-                        height: (screenHeight(context) ~/  homeScreenController.rows.value).toDouble(),
-                        margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          color:homeScreenController.highlightedAlphabetsIndexList.contains(index)?Colors.blue:Colors.red,
-                        ),
-                        child: FittedBox(
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Center(
-                              child: Text(homeScreenController.generatedChars[index],style: TextStyle(color: Colors.white),),
+                    () => Container(
+                      width: (screenWidth(context) ~/
+                              homeScreenController.column.value)
+                          .toDouble(),
+                      height: (screenHeight(context) ~/
+                              homeScreenController.rows.value)
+                          .toDouble(),
+                      margin:
+                          EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: homeScreenController
+                                .highlightedAlphabetsIndexList
+                                .contains(index)
+                            ? Colors.blue
+                            : Colors.red,
+                      ),
+                      child: FittedBox(
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Center(
+                            child: Text(
+                              homeScreenController.generatedChars[index],
+                              style: TextStyle(color: Colors.white),
                             ),
                           ),
                         ),
                       ),
+                    ),
                   );
-
                 },
                 itemCount: homeScreenController.generatedChars.length,
-              ):Center(
-              child: Text("Please Tap on refresh icon"),
-            ),
-            ),
-        ),
+              )
+            : Center(
+                child: Text("Please Tap on refresh icon"),
+              ),
+                ),
       ),
-
     );
   }
 }
